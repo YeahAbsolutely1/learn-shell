ebf7e784d91f3186a25c2f3879d9c37a5a8ce866
Eliezer <eliezersov@gmail.com>
2023-02-08 17:41:18 +0530

    added a new comment (Color code syntax)

--- 02. Print.sh
+++ 02. Print.sh
@@ -1,3 +1,19 @@
 echo Hello World
 echo ****Hello World****
 echo "****Hello World****"
+
+# Color code syntax  : echo -e "\e[CoLmWelcome to NewYork\e[0m"
+# Red       - 31     : echo -e "\e[31m Welcome to New York\e[0m"
+# Green     - 32     : echo -e "\e[32m Welcome to New York\e[0m"
+# Yellow    - 33     : echo -e "\e[33m Welcome to New York\e[0m"
+# Blue      - 34     : echo -e "\e[34m Welcome to New York\e[0m"
+# Magenta   - 35     : echo -e "\e[35m Welcome to New York\e[0m"
+# Cyan      - 36     : echo -e "\e[36m Welcome to New York\e[0m"
+
+
+echo -e "\e[31m Welcome to New York\e[0m"
+echo -e "\e[32m Welcome to New York\e[0m"
+echo -e "\e[33m Welcome to New York\e[0m"
+echo -e "\e[34m Welcome to New York\e[0m"
+echo -e "\e[35m Welcome to New York\e[0m"
+echo -e "\e[36m Welcome to New York\e[0m"
