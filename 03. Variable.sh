2d41a5c53da3a045d489cd557e2842a53a17c562
Eliezer <eliezersov@gmail.com>
2023-02-08 17:51:33 +0530

    Variable 1st script

---
+++ 03. Variable.sh
@@ -0,0 +1,7 @@
+# If we assign a name to a set of data is called as Variable
+
+Coach=Rahul Dravid
+Team=Indian Test Cricket Team
+
+echo Coach name - $Rahul Dravid
+echo Team name - ${Indian Test Cricket Team}
