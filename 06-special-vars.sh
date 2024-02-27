d7ece34e972d7d0d5ecd03abcf03d49136c8c18e
Eliezer <eliezersov@gmail.com>
2024-02-28 01:05:09 +0530

    special-vars.sh

--- 06-special-vars.sh
+++ 06-special-vars.sh
@@ -1,3 +1,4 @@
+echo "Following values from script"
 echo "0 - $0"
 echo "1 - $1"
 echo "2 - $2"
@@ -5,3 +6,15 @@ echo "3 - $3"
 echo "* - $*"
 echo "@ - $@"
 echo "# - $#"
+
+example() {Following values from function
+echo "0 - $0"
+echo "1 - $1"
+echo "2 - $2"
+echo "3 - $3"
+echo "* - $*"
+echo "@ - $@"
+echo "# - $#"
+}
+
+example
