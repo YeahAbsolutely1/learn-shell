f36f256ea9cad3fdd395500f9d539968dbc35004
Eliezer <eliezersov@gmail.com>
2024-02-27 23:25:05 +0530

    Function.sh Shell-Scripting

---
+++ 05-function.sh
@@ -0,0 +1,13 @@
+# Functions are like Variables.
+# Variable - When we assign a name to a set of data then it is a variable.
+# Function - When we assign a name to a set of commands then it is a function.
+
+#Function Declare
+
+example() {
+  echo I am an example.
+}
+
+## Call a function
+
+example
